5fb1372e104de766cdbe8decbd767445e1d6a2d3
Rocky <sarowarhossainrocky1@gmail.com>
2017-12-01 22:25:43 +0600

    findSimpleGene method definition added which will find gene from given dna

---
+++ src/Part1.java
@@ -0,0 +1,23 @@
+
+public class Part1 {
+
+	public String findSimpleGene(String dna){
+        int startIndex = dna.indexOf("ATG");
+        int stopIndex = dna.indexOf("TAA", startIndex + 3);
+        while(stopIndex != -1){
+            if((stopIndex - startIndex) % 3 == 0){
+                return dna.substring(startIndex, stopIndex + 3);
+            }
+            else{
+                stopIndex = dna.indexOf("TAA", stopIndex + 1);
+            }
+        }
+        return "";
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
