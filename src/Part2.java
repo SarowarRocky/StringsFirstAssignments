a33cabb7827eb6e603f8826a506cf6e14388cd34
Rocky <sarowarhossainrocky1@gmail.com>
2017-12-02 12:53:13 +0600

    This is the last point for Part2.

--- src/Part2.java
+++ src/Part2.java
@@ -4,43 +4,65 @@
 
 public class Part2 {
 
-	public String findSimpleGene(String dna){
-        int startIndex = dna.indexOf("ATG");
-        int stopIndex = dna.indexOf("TAA", startIndex + 3);
+	public String findSimpleGene(String dna, String startCodon, String stopCodon){
+        int startIndex = dna.indexOf(startCodon);
+        int stopIndex = dna.indexOf(stopCodon, startIndex + 3);
         while(stopIndex != -1){
             if((stopIndex - startIndex) % 3 == 0){
                 return dna.substring(startIndex, stopIndex + 3);
             }
             else{
-                stopIndex = dna.indexOf("TAA", stopIndex + 1);
+                stopIndex = dna.indexOf(stopCodon, stopIndex + 1);
             }
         }
         return "";
     }
 	
 	
-	 void testSimpleGene(){
-	        String dna = "AATGCGTAATTAATCG";
-	        System.out.println("DNA strand is " + dna);
-	        String gene = findSimpleGene(dna);
-	        System.out.println("Gene is " + gene);
-	        dna = "CGATGGTTGATAAGCCTAAGCTATAA";
-	        System.out.println("DNA strand is " + dna);
-	        gene = findSimpleGene(dna);
-	        System.out.println("Gene is " + gene);
-	        dna = "CGATGGTTGATAAGCCTAAGCTAAA";
-	        System.out.println("DNA strand is " + dna);
-	        gene = findSimpleGene(dna);
-	        System.out.println("Gene is " + gene);
-	    }
-	
-	
+	void testSimpleGene(){
+        String dna = "ATGGGTTAAGTC";
+        char ch = dna.charAt(0);
+        String startCodon = "", stopCodon = "";
+        if(Character.isUpperCase(ch)){
+            startCodon = "ATG";
+            stopCodon = "TAA";
+        }
+        else{
+            startCodon = "atg";
+            stopCodon = "taa";
+        }
+        
+        System.out.println("DNA strand is " + dna);
+        String gene = findSimpleGene(dna, startCodon, stopCodon);
+        System.out.println("Gene is " + gene);
+        
+        dna = "gatgctataat";
+        ch = dna.charAt(0);
+        if(Character.isUpperCase(ch)){
+            startCodon = "ATG";
+            stopCodon = "TAA";
+        }
+        else{
+            startCodon = "atg";
+            stopCodon = "taa";
+        }
+        System.out.println("DNA strand is " + dna);
+        gene = findSimpleGene(dna, startCodon, stopCodon);
+        System.out.println("Gene is " + gene);
+        /*dna = "CGATGGTTGATAAGCCTAAGCTAAA";
+        System.out.println("DNA strand is " + dna);
+        gene = findSimpleGene(dna, startCodon, stopCodon);
+        System.out.println("Gene is " + gene);*/
+    }
+
 	 
 	 
 	 
 	 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Part2 obj = new Part2();
+		obj.testSimpleGene();
 
 	}
 
