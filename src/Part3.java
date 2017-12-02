6a661465298b3995cbc581efc33370ab4b2e0719
Rocky <sarowarhossainrocky1@gmail.com>
2017-12-02 13:01:55 +0600

    Part3 first thing working.

---
+++ src/Part3.java
@@ -0,0 +1,32 @@
+
+public class Part3 {
+
+	 public boolean twoOccurrences(String stringa, String stringb){
+	        int curIndex = 0, count = 0;
+	        int l = stringa.length();
+	        curIndex = stringb.indexOf(stringa, curIndex);
+	        while(true){
+	            if(curIndex != -1){
+	                count++;
+	                if(count == 2){
+	                    return true;
+	                }
+	            }
+	            else{
+	                return false;
+	            }
+	            curIndex = stringb.indexOf(stringa, curIndex + l);
+	        }
+	    }
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
